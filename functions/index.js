b6454cc97c11dbbb45b04fa8045baba55745af1d
Ken Kawamoto <ken@kawamoto.co.uk>
2023-04-29 11:25:17 +1000

    Test different prompt

--- functions/index.js
+++ functions/index.js
@@ -49,8 +49,8 @@ exports.processImage = functions
             // prompt: "How are you?",
             messages: [
                 { "role": "system", "content": "You are a helpful assistant." },
-                { "role": "user", "content": "Who won the world series in 2020?" },
-                { "role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020." },
+                { "role": "user", "content": "Who won the world series in 2027?" },
+                { "role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2027." },
                 { "role": "user", "content": "Where was it played?" }
             ],
             // temperature: 0.6,
